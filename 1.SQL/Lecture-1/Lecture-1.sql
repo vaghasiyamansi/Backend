48e96fee4cd38e09dc54881591a727625fe6555c
mansi <vaghasiyamaahigmail.com>
2024-06-28 08:28:04 +0530

    28/06/2024

---
+++ 1.SQL/Lecture-1/Lecture-1.sql
@@ -0,0 +1,25 @@
+1. connect mysql 
+-- \sql
+-- \connect root@localhost:3306
+
+2.show all databases command 
+-- show databases;
+
+3. create database command 
+-- create database <databasename>;
+
+4. use database command 
+-- use <databasename>;
+
+5.delete database command
+-- drop database <databasename>;
+
+6.show particular database tables
+-- show tables;
+
+7. create new table command
+-- create table user(firstname varchar(50), lastname varchar(50) ,  email varchar(50) , gender varchar(10) , mobileNo int);
+
+8.describe or show perticular table(row and column)
+-- desc user;
+-- describe user;
